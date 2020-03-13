281e58b4466320a6b9001abf0c1b6c9737ea1951
Vladislav Supalov <mail@vsupalov.com>
2020-03-13 16:38:40 +0100

    Add apps and enable the custom user model

--- PROJECTNAME/settings.py
+++ PROJECTNAME/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'APPNAME',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -99,6 +101,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+AUTH_USER_MODEL = 'users.User'
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
