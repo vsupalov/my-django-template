e5d96d4e0c9dd875890cafb8e201b330f025be32
Vladislav Supalov <mail@vsupalov.com>
2020-11-22 19:09:45 +0100

    Add example model related code

--- APPNAME/models.py
+++ APPNAME/models.py
@@ -2,3 +2,11 @@ from django.db import models
 #from django.conf import settings
 
 # See https://vsupalov.com/quick-django-refresher-crash-course/#create-models-in-your-app
+
+#class Thing(models.Model):
+#    name = models.CharField(max_length=200) # blank=True
+#    date = models.DateTimeField('date published') # blank=True, null=True
+#    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+#
+#    def __str__(self):
+#        return "{}".format(self.name)
